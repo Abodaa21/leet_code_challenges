a6b2d69c73dfc44a9448f80b61ce4a7173b1a7ac
redhood <abd7samad7@gmail.com>
2026-07-08 13:16:27 +0100

    hi

---
+++ monotonic_array/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if len(nums) == 0:
+            return True
+        previous = nums[0]
+        first_time = 1
+        increasing = 0
+        decreasing = 0
+        for n in nums:
+            if first_time:
+                if n < previous:
+                    first_time = 0
+                    decreasing = 1
+                elif n > previous:
+                    first_time = 0
+                    increasing = 1
+            if (decreasing and n > previous) or (increasing and n < previous):
+                return False
+            previous = n
+        return True
+
+print(Solution().isMonotonic([1,2,2,3]))
+                
