94c74cb8d0e269dd9aaf7d4a78c128d6d5a00e49
redhood <abd7samad7@gmail.com>
2026-07-17 04:46:44 +0100

    HI

--- Zigzag Conversion/solution.py
+++ Zigzag Conversion/solution.py
@@ -1,24 +1,20 @@
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
+    def convert(self: "Solution", s: str, numRows: int) -> str:
         i = 0
         lst = [[] for _ in range(numRows)]
-        zigzag = 0
         while i < len(s):
-            zigzag += 1
-            if zigzag % 2 != 0:
-                for n in range(numRows):
-                    if i < len(s):
-                        lst[n].append(s[i])
-                        i += 1
-            else:
-                for n in range(numRows):
-                    if n % 2 != 0 and i < len(s):
-                        lst[n].append(s[i])
-                        i += 1
+            for n in range(numRows):
+                if i < len(s):
+                    lst[n].append(s[i])
+                    i += 1
+            for n in range(numRows):
+                if i < len(s) and numRows - n - 2 > 0:
+                    lst[numRows - n - 2].append(s[i])
+                    i += 1
         string = ""
         for n in range(numRows):
             string += "".join(lst[n])
-        print(string)
-                
+        return string
+
+
 Solution().convert("PAYPALISHIRING", 4)
-                 
