4ae6d9c3346a45994a19dd7697448da1d708b956
redhood <abd7samad7@gmail.com>
2026-07-30 02:25:30 +0100

    HI

---
+++ C_language/3Sum_Closest/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+int threeSumClosest(int* nums, int numsSize, int target) {
+    int i;
+
+    i = 0;
+    while (i < numsSize)
+    {
+        
+    }
+}
